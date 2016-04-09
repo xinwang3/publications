e004a65454aaa3d9b9d570037b702e3fe3532253
xinwang3 <xin.wang.3@stonybrook.edu>
2016-04-09 16:48:01 -0400

    Update pubapp.js

--- pubapp.js
+++ pubapp.js
@@ -2,23 +2,4 @@ console.log('pubapp.js loaded');
 here
 test 
 
-(function(){ // wrapping everything within an annonymous function
-    var h = '<h3 style="color:brown">Faculty Publications</h3>'
-    
-    h += 'Last Name: <input id="lastname" size=4 value="70"> <br> '
-    h += '<button id="subButton" type="button" >submit</button> ';
-    
-     
-    
-    if(typeof(sbmApps)=='function'){ // if sbmApps library was loaded
-        sbmApps.render(h)
-    }else{ // if fib.js is being called without any help
-        var dv = document.createElement('div')
-        dv.innerHTML = h
-        document.body.appendChild(dv)
-    }
-    
-     
-     
-})()
-Status API Training Shop Blog About
+

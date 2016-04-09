c61afadd4353b0f6ce42a4e554463adc3ce3dc32
xinwang3 <xin.wang.3@stonybrook.edu>
2016-04-09 17:02:57 -0400

    Update pubapp.js

--- pubapp.js
+++ pubapp.js
@@ -1,5 +1,30 @@
-console.log('pubapp.js loaded');
-here
-test 
+console.log('cocao.js loaded');
 
-
+(function(){ // wrapping everything within an annonymous function
+    var h = '<h3 style="color:brown">Dark Chocolate Survey</h3>'
+    
+    h += 'What % of cocoa do you like the most: <input id="cocaoPct" size=4 value="70"> <br> '
+    h += '<button id="subButton" type="button" >submit</button> '
+    
+     
+    
+ 
+        var dv = document.createElement('div')
+        dv.innerHTML = h
+        document.body.appendChild(dv)
+ 
+    
+    subButton.onclick=function(){
+         
+        var x = parseInt(cocaoPct.value)
+        if (x >=87) {
+            alert( "Thank you, isn't " + x +" %  too dark!")
+        }
+        else{
+            alert("Thank you, I like " + x +" %  too.");
+        }
+    }
+     
+})()
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
